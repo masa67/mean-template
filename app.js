ac9ea874ca210bfa542d2776f5888ba0d4347dd7
Matti Sangi <matti.sangi@pp.inet.fi>
2014-08-10 14:03:59 +0300

    v.0.2.1

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var bodyParser = require('body-parser');
 
 // New Code
 var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/mean-template');
+var monk = require('mongoskin');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,12 +24,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
 
 app.use('/', routes);
 app.use('/users', users);
