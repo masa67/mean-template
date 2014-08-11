eddd3843f4924aa70826c971e481672d752c337a
Matti Sangi <matti.sangi@pp.inet.fi>
2014-08-11 21:27:44 +0300

    v.0.4.0

--- db-init/mean-template-init.js
+++
@@ -1,6 +0,0 @@
-use mean-template
-db.usercollection.insert({ "username" : "testuser1", "email" : "testuser1@testdomain.com" })
-db.usercollection.insert({ "username" : "testuser2", "email" : "testuser2@testdomain.com" })
-db.usercollection.insert({ "username" : "testuser3", "email" : "testuser3@testdomain.com" })
-
-
