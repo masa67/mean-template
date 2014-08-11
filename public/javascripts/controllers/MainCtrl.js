4edd31ad89f51e5680a8a0f6bf91b7b9b4c01b6b
Matti Sangi <matti.sangi@pp.inet.fi>
2014-08-11 16:09:53 +0300

    v.0.3.4

--- public/javascripts/controllers/MainCtrl.js
+++ public/javascripts/controllers/MainCtrl.js
@@ -5,4 +5,22 @@ angular.module('MainCtrl', []).controller('MainController',
     users.getUserList(function(data) {
         $scope.userList = data;
     });
+
+    $scope.userDetail = {};
+    $('#userList table tbody').on('click', 'td a.linkshowuser', function(event) {
+            // Prevent Link from Firing
+            event.preventDefault();
+
+            // Retrieve username from link rel attribute
+            var thisUserName = $(this).attr('rel');
+
+            // Get Index of object based on id value
+            var arrayPosition = $scope.userList.map(function(arrayItem) { return arrayItem.username; })
+                .indexOf(thisUserName);
+
+            // Get our User Object
+            $scope.userDetail = $scope.userList[arrayPosition];
+            $scope.$apply();
+        });
 }]);
+
